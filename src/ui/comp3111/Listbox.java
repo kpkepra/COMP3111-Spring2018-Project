b4fd5a964497b90f1355dfadfef496d69dd5bf42
kp-kepra <kevinpr153@gmail.com>
2018-05-02 02:19:52 +0800

    fixed bugs in pie typecasting, added radiobutton switch feature, corgi and csv multiple file hosting. Embedded excel wip

--- src/ui/comp3111/Listbox.java
+++ src/ui/comp3111/Listbox.java
@@ -1,5 +1,9 @@
 package ui.comp3111;
 
+import java.io.File;
+import java.util.ArrayList;
+
+import core.comp3111.MyFileExtenstion.CorgiObj;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.ListView;
@@ -7,15 +11,36 @@ import javafx.scene.layout.Pane;
 
 public class Listbox {
 	private static Pane pane;
+	private static ObservableList<String> filenames = FXCollections.observableArrayList();
+	private static ObservableList<String> corginames = FXCollections.observableArrayList();
+	private static ArrayList<File> files = new ArrayList<File>();
+	private static ArrayList<CorgiObj> corgis = new ArrayList<CorgiObj>();
+
 	
 	public static Pane pane() {
 		ListView<String> list = new ListView<String>();
-		ObservableList<String> items = FXCollections.observableArrayList(
-				"Dataset1", "Dataset2", "Dataset3", "Dataset4");
-		list.setItems(items);
+		list.setItems(filenames);
 		
 		pane = new Pane();
 		pane.getChildren().add(list);
 		return pane;
+	}
+	
+	/* addDataset
+	 * Adds one dataset to the list
+	 */
+	public static void addDataset(File file) {
+		filenames.add(file.getName());
+		files.add(file);
+		System.out.println(files.size());
+	}
+	
+	/* addCorgi
+	 * Adds one file extension to the list
+	 */
+	public static void addCorgi(CorgiObj corgi) {
+		corginames.add(corgi.getName());
+		corgis.add(corgi);
+		System.out.println(corgis.size());
 	}
 }
