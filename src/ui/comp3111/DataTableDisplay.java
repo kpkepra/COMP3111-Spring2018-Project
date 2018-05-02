1ccb386da9a47a4d40b1463682a059edfe1702b3
kp-kepra <kevinpr153@gmail.com>
2018-05-02 16:55:05 +0800

    wip

--- src/ui/comp3111/DataTableDisplay.java
+++ src/ui/comp3111/DataTableDisplay.java
@@ -12,48 +12,48 @@ import javafx.scene.control.*;
 
 public class DataTableDisplay {
 	  private static File file;
+	  private static DataTable table;
 	
-	  public TableView displayTable(DataTable table) {
-	  TableView<Integer> datasetTable = new TableView();
-	  datasetTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-	
-	  for (int i = 0; i < table.getNumRow(); i++) {
-	      datasetTable.getItems().add(i);
-	  }
-	
-	  for (String colName : table.getColNames()) {
-	      DataColumn dataColumn = table.getCol(colName);
-	      Object[] data = dataColumn.getData();
-	
-	      if (Objects.equals(dataColumn.getTypeName(), DataType.TYPE_STRING)) {
-	          TableColumn<Integer, String> column = new TableColumn<>(colName);
-	
-	          column.setCellValueFactory(cellData -> {
-	              Integer rowIndex = cellData.getValue();
-	
-	              return new ReadOnlyStringWrapper((String) data[rowIndex]);
-	
-	          });
-	          column.prefWidthProperty().bind(datasetTable.widthProperty().divide(table.getNumCol()));
-	          datasetTable.getColumns().add(column);
-	      }
-	      if (Objects.equals(dataColumn.getTypeName(), DataType.TYPE_NUMBER)) {
-	          TableColumn<Integer, Number> column = new TableColumn<>(colName);
-	
-	          column.setCellValueFactory(cellData -> {
-	              Integer rowIndex = cellData.getValue();
-	
-	              return new ReadOnlyFloatWrapper(((Number)data[rowIndex]).floatValue());
-	          });
-	          column.prefWidthProperty().bind(datasetTable.widthProperty().divide(table.getNumCol()));
-	          datasetTable.getColumns().add(column);
-	      }
-	  }
-	
-	  return datasetTable;
+	  public TableView displayTable() {
+		  TableView<Integer> datasetTable = new TableView();
+		  datasetTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+		
+		  for (int i = 0; i < table.getNumRow(); i++) {
+		      datasetTable.getItems().add(i);
+		  }
+		
+		  for (String colName : table.getColNames()) {
+		      DataColumn dataColumn = table.getCol(colName);
+		      Object[] data = dataColumn.getData();
+		
+		      if (Objects.equals(dataColumn.getTypeName(), DataType.TYPE_STRING)) {
+		          TableColumn<Integer, String> column = new TableColumn<>(colName);
+		
+		          column.setCellValueFactory(cellData -> {
+		              Integer rowIndex = cellData.getValue();
+		
+		              return new ReadOnlyStringWrapper((String) data[rowIndex]);
+		
+		          });
+		          column.prefWidthProperty().bind(datasetTable.widthProperty().divide(table.getNumCol()));
+		          datasetTable.getColumns().add(column);
+		      }
+		      if (Objects.equals(dataColumn.getTypeName(), DataType.TYPE_NUMBER)) {
+		          TableColumn<Integer, Number> column = new TableColumn<>(colName);
+		
+		          column.setCellValueFactory(cellData -> {
+		              Integer rowIndex = cellData.getValue();
+		
+		              return new ReadOnlyFloatWrapper(((Number)data[rowIndex]).floatValue());
+		          });
+		          column.prefWidthProperty().bind(datasetTable.widthProperty().divide(table.getNumCol()));
+		          datasetTable.getColumns().add(column);
+		      }
+		  }
+		
+		  return datasetTable;
 	  }
 	  
 	  public static void setTable(File newFile) {
-		  
 	  }
 }
