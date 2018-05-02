768b7fcf543d1d23dddc8a35f7f040570bf13434
Adrian Prawira Susanto <33570557+apsusanto@users.noreply.github.com>
2018-05-02 19:07:36 +0800

    Revert "Dev kevin"

--- src/ui/comp3111/Listbox.java
+++ src/ui/comp3111/Listbox.java
@@ -1,64 +1,21 @@
 package ui.comp3111;
 
-import java.io.File;
-import java.util.ArrayList;
-
-import core.comp3111.MyFileExtenstion.CorgiObj;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.scene.control.ListView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
 public class Listbox {
 	private static Pane pane;
 	
-	// Datasets and Corgis to internal memory
-	private static ObservableList<String> filenames = FXCollections.observableArrayList();
-	private static ObservableList<String> corginames = FXCollections.observableArrayList();
-	private static ArrayList<File> files = new ArrayList<File>();
-	private static ArrayList<CorgiObj> corgis = new ArrayList<CorgiObj>();
-	
-	// List for Display
-	private static ListView<String> list = new ListView<String>();
-
-	
 	public static Pane pane() {
-		list.setItems(filenames);
+		ListView<String> list = new ListView<String>();
+		ObservableList<String> items = FXCollections.observableArrayList(
+				"Dataset1", "Dataset2", "Dataset3", "Dataset4");
+		list.setItems(items);
 		
 		pane = new Pane();
 		pane.getChildren().add(list);
-		initHandlers();
-		
 		return pane;
-	}
-	
-	/* addDataset
-	 * Adds one dataset to the list
-	 */
-	public static void addDataset(File file) {
-		filenames.add(file.getName());
-		files.add(file);
-	}
-	
-	/* addCorgi
-	 * Adds one file extension to the list
-	 */
-	public static void addCorgi(CorgiObj corgi) {
-		corginames.add(corgi.getName());
-		corgis.add(corgi);
-		System.out.println(corgis.size());
-	}
-	
-	static void initHandlers() {
-		list.setOnMouseClicked(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent arg0) {
-				int idx = list.getSelectionModel().getSelectedIndex();
-				DataTableDisplay.setTable(files.get(idx));
-			}
-			
-		});
 	}
 }
