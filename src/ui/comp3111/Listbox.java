1fc414a3a313723577ee3c2d6108a6471a1a2d5a
kp-kepra <kevinpr153@gmail.com>
2018-05-02 14:47:21 +0800

    displaying embedded excel wip

--- src/ui/comp3111/Listbox.java
+++ src/ui/comp3111/Listbox.java
@@ -6,23 +6,31 @@ import java.util.ArrayList;
 import core.comp3111.MyFileExtenstion.CorgiObj;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
 public class Listbox {
 	private static Pane pane;
+	
+	// Datasets and Corgis to internal memory
 	private static ObservableList<String> filenames = FXCollections.observableArrayList();
 	private static ObservableList<String> corginames = FXCollections.observableArrayList();
 	private static ArrayList<File> files = new ArrayList<File>();
 	private static ArrayList<CorgiObj> corgis = new ArrayList<CorgiObj>();
+	
+	// List for Display
+	private static ListView<String> list = new ListView<String>();
 
 	
 	public static Pane pane() {
-		ListView<String> list = new ListView<String>();
 		list.setItems(filenames);
 		
 		pane = new Pane();
 		pane.getChildren().add(list);
+		initHandlers();
+		
 		return pane;
 	}
 	
@@ -42,5 +50,17 @@ public class Listbox {
 		corginames.add(corgi.getName());
 		corgis.add(corgi);
 		System.out.println(corgis.size());
+	}
+	
+	static void initHandlers() {
+		list.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent arg0) {
+				int idx = list.getSelectionModel().getSelectedIndex();
+				DataTableDisplay.setTable(files[idx]);
+				System.out.println(list.getSelectionModel().getSelectedIndex());
+			}
+			
+		});
 	}
 }
