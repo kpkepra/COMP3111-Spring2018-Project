ed5f2d7f9220ce5509b4fcd61acb17e23c98093e
kp-kepra <kevinpr153@gmail.com>
2018-05-03 00:59:54 +0800

    animated chart done

---
+++ src/core/comp3111/AnimatedChart.java
@@ -0,0 +1,32 @@
+package core.comp3111;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/*
+ * 
+ */
+
+public class AnimatedChart extends Chart implements Serializable {
+	private 
+	private ArrayList<String> numCols;
+	private String x;
+	private String y;
+	private int timeRange;
+	
+	public AnimatedChart(DataTable dt) throws ChartException {
+		data = dt;
+		
+	}
+	
+	/*
+	 * Requirements : Line Chart, 1 Numeric Column
+	 */
+	@Override
+	protected boolean isLegal() {
+		
+		return false;
+	}
+	
+	
+}
