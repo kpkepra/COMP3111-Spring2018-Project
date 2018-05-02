6f7658ee986fa17334e68e1610438839fa55e88d
kp-kepra <kevinpr153@gmail.com>
2018-05-03 02:04:09 +0800

    save & load

--- src/ui/comp3111/Listbox.java
+++ src/ui/comp3111/Listbox.java
@@ -3,6 +3,9 @@ package ui.comp3111;
 import java.io.File;
 import java.util.ArrayList;
 
+import core.comp3111.CSVReader;
+import core.comp3111.DataTable;
+import core.comp3111.DataTableTransformer;
 import core.comp3111.Transform;
 import core.comp3111.MyFileExtenstion.CorgiObj;
 import javafx.collections.FXCollections;
@@ -18,7 +21,7 @@ public class Listbox {
 	// Datasets and Corgis to internal memory
 	private static ObservableList<String> filenames = FXCollections.observableArrayList();
 	private static ObservableList<String> corginames = FXCollections.observableArrayList();
-	private static ArrayList<File> files = new ArrayList<File>();
+	private static ArrayList<DataTable> tables = new ArrayList<DataTable>();
 	private static ArrayList<CorgiObj> corgis = new ArrayList<CorgiObj>();
 	
 	// List for Display
@@ -39,7 +42,17 @@ public class Listbox {
 	 */
 	public static void addDataset(File file) {
 		filenames.add(file.getName());
-		files.add(file);
+		CSVReader csv = new CSVReader(file.getName());
+		csv.readALL(0);
+		csv.readField();
+		DataTable table = DataTableTransformer.transform(csv);
+		tables.add(table);
+		
+	}
+	
+	public static void addDataset(DataTable table) {
+		filenames.add(table.toString());
+		tables.add(table);
 	}
 	
 	/* addCorgi
@@ -48,16 +61,25 @@ public class Listbox {
 	public static void addCorgi(CorgiObj corgi) {
 		corginames.add(corgi.getName());
 		corgis.add(corgi);
+		ArrayList<DataTable> dts = corgi.getDts();
+		dts.forEach(dt -> {
+			filenames.add(corgi.getName());
+			tables.add(dt);
+		});
+	}
+	
+	public static ArrayList<DataTable> getTables() {
+		return tables;
 	}
 	
 	static void initHandlers() {
 		list.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent arg0) {
-				int idx = list.getSelectionModel().getSelectedIndex();
-				DataTableDisplay.setTable(files.get(idx));
-				MainScreen.tfDisplay = new TransformDisplay(new Transform(DataTableDisplay.getDT()));
+				DataTable table = tables.get(list.getSelectionModel().getSelectedIndex());
+				DataTableDisplay.setTable(table);
 				
+				MainScreen.tfDisplay = new TransformDisplay(new Transform(DataTableDisplay.getDT()));
 				MainScreen.rightc.getChildren().remove(1);
 				MainScreen.filterPane = MainScreen.tfDisplay.splitFilter();
 				MainScreen.rightc.getChildren().add(1, MainScreen.filterPane);
