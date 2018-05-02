0538c21d421e0cc35e3959e9565225f53fb5415e
kp-kepra <kevinpr153@gmail.com>
2018-05-02 17:46:34 +0800

    staged

--- src/ui/comp3111/DataTableDisplay.java
+++ src/ui/comp3111/DataTableDisplay.java
@@ -3,8 +3,10 @@ package ui.comp3111;
 import java.io.File;
 import java.util.Objects;
 
+import core.comp3111.CSVReader;
 import core.comp3111.DataColumn;
 import core.comp3111.DataTable;
+import core.comp3111.DataTableTransformer;
 import core.comp3111.DataType;
 import javafx.beans.property.ReadOnlyFloatWrapper;
 import javafx.beans.property.ReadOnlyStringWrapper;
@@ -12,48 +14,56 @@ import javafx.scene.control.*;
 
 public class DataTableDisplay {
 	  private static File file;
-//	  private static DataTable table;
+	  private static DataTable table = new DataTable();
+	  private static CSVReader csv;
 	
-	  public TableView displayTable(DataTable table) {
+	  public static TableView displayTable() {
 		  TableView<Integer> datasetTable = new TableView();
 		  datasetTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-		
-		  for (int i = 0; i < table.getNumRow(); i++) {
-		      datasetTable.getItems().add(i);
-		  }
-		
-		  for (String colName : table.getColNames()) {
-		      DataColumn dataColumn = table.getCol(colName);
-		      Object[] data = dataColumn.getData();
-		
-		      if (Objects.equals(dataColumn.getTypeName(), DataType.TYPE_STRING)) {
-		          TableColumn<Integer, String> column = new TableColumn<>(colName);
-		
-		          column.setCellValueFactory(cellData -> {
-		              Integer rowIndex = cellData.getValue();
-		
-		              return new ReadOnlyStringWrapper((String) data[rowIndex]);
-		
-		          });
-		          column.prefWidthProperty().bind(datasetTable.widthProperty().divide(table.getNumCol()));
-		          datasetTable.getColumns().add(column);
-		      }
-		      if (Objects.equals(dataColumn.getTypeName(), DataType.TYPE_NUMBER)) {
-		          TableColumn<Integer, Number> column = new TableColumn<>(colName);
-		
-		          column.setCellValueFactory(cellData -> {
-		              Integer rowIndex = cellData.getValue();
-		
-		              return new ReadOnlyFloatWrapper(((Number)data[rowIndex]).floatValue());
-		          });
-		          column.prefWidthProperty().bind(datasetTable.widthProperty().divide(table.getNumCol()));
-		          datasetTable.getColumns().add(column);
-		      }
+		  if (table.getNumRow() > 0 && table.getNumCol() > 0) {
+			  for (int i = 0; i < table.getNumRow(); i++) {
+			      datasetTable.getItems().add(i);
+			  }
+			
+			  for (String colName : table.getColNames()) {
+			      DataColumn dataColumn = table.getCol(colName);
+			      Object[] data = dataColumn.getData();
+			
+			      if (Objects.equals(dataColumn.getTypeName(), DataType.TYPE_STRING)) {
+			          TableColumn<Integer, String> column = new TableColumn<>(colName);
+			
+			          column.setCellValueFactory(cellData -> {
+			              Integer rowIndex = cellData.getValue();
+			
+			              return new ReadOnlyStringWrapper((String) data[rowIndex]);
+			
+			          });
+			          column.prefWidthProperty().bind(datasetTable.widthProperty().divide(table.getNumCol()));
+			          datasetTable.getColumns().add(column);
+			      }
+			      if (Objects.equals(dataColumn.getTypeName(), DataType.TYPE_NUMBER)) {
+			          TableColumn<Integer, Number> column = new TableColumn<>(colName);
+			
+			          column.setCellValueFactory(cellData -> {
+			              Integer rowIndex = cellData.getValue();
+			
+			              return new ReadOnlyFloatWrapper(((Number)data[rowIndex]).floatValue());
+			          });
+			          column.prefWidthProperty().bind(datasetTable.widthProperty().divide(table.getNumCol()));
+			          datasetTable.getColumns().add(column);
+			      }
+			  }
 		  }
 		
 		  return datasetTable;
 	  }
 	  
 	  public static void setTable(File newFile) {
+		  file = newFile;
+		  csv = new CSVReader(file.getName());
+		  csv.readALL(0);
+		  csv.readField();
+		  table = DataTableTransformer.transform(csv);
+		  System.out.println(table.getNumCol());
 	  }
 }
