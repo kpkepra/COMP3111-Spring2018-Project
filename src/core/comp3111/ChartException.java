623b9486e51844bf0da1e741c60dd2de60a4ea6e
Adrian Prawira Susanto <apsusanto@connect.ust.hk>
2018-05-04 13:52:24 +0800

    Added alert display for invalid chart

--- src/core/comp3111/ChartException.java
+++ src/core/comp3111/ChartException.java
@@ -1,6 +1,6 @@
 package core.comp3111;
 
-public class ChartException extends Exception {
+public class ChartException extends RuntimeException {
 
     public ChartException(String message) {
         super("ChartException: " + message);
